83c47a392110052bee6409e51733efef9bc061ea
Valentyna <ext-valentyna.lysenok@here.com>
2023-06-03 22:15:26 +0300

    HW2: add job1 save_sales_to_local_disk functionality

---
+++ job1/bll/sales_api.py
@@ -0,0 +1,10 @@
+from job1.dal import local_disk, sales_api
+
+
+def save_sales_to_local_disk(date: str, page: str, raw_dir: str) -> None:
+    # 1. get data from the API
+    json_content = sales_api.get_sales(date=date, page=page)
+    # 2. save data to disk
+    local_disk.save_to_disk(json_content=json_content, path=raw_dir)
+
+    print("\tI'm in get_sales(...) function!")

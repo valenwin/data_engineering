b03bf8529d9d9132a71451a1019db177de086cc6
Valentyna <ext-valentyna.lysenok@here.com>
2023-06-03 22:15:26 +0300

    HW2: add job1 save_sales_to_local_disk functionality

--- job1/dal/local_disk.py
+++ job1/dal/local_disk.py
@@ -1,7 +1,14 @@
 import json
+import os
 from typing import List, Dict, Any
 
 
 def save_to_disk(json_content: List[Dict[str, Any]], path: str) -> None:
-    with open(path, "w") as file:
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+    file_date = path.split('/')[-1]
+    file_path = f"{path}/{file_date}.json"
+
+    with open(file_path, "w") as file:
         json.dump(json_content, file)
--- job1/main.py
+++ job1/main.py
@@ -5,19 +5,37 @@ and trigger business logic layer
 from flask import Flask, request
 from flask import typing as flask_typing
 
-from job1.dal import sales_api
-
+from job1.bll.sales_api import save_sales_to_local_disk
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def main() -> flask_typing.ResponseReturnValue:
-    query_parameters = request.args
-    date = query_parameters.get("date", "2022-08-09")
-    page = query_parameters.get("page", "1")
+    """
+       Controller that accepts command via HTTP and
+       trigger business logic layer
 
-    return sales_api.get_sales(date=date, page=page)
+       Proposed POST body in JSON:
+       {
+         "date": "2022-08-09",
+         "raw_dir": "file_storage/raw/sales/2022-08-09"
+       }
+    """
+    input_data: dict = request.json
+    date = input_data.get('date')
+    raw_dir = input_data.get('raw_dir')
+
+    if not date:
+        return {
+            "message": "date parameter missed",
+        }, 400
+
+    save_sales_to_local_disk(date=date, page='1', raw_dir=raw_dir)
+
+    return {
+        "message": "Data retrieved successfully from API",
+    }, 201
 
 
 if __name__ == "__main__":

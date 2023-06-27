929ffab4be9eef34c9e171c1a1d0a3be18ec25fa
Valentyna <ext-valentyna.lysenok@here.com>
2023-06-27 20:47:55 +0300

    HW4: re-write logic from HW2 to airflow dags

--- dags/demo.py
+++ dags/demo.py
@@ -1,6 +1,9 @@
+import json
+import os
 from typing import List, Dict, Any
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests
+import pandas as pd
 
 from airflow import DAG
 from airflow.models import Variable
@@ -8,6 +11,10 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.dates import days_ago
 
+start_date = datetime(2022, 8, 9)
+end_date = datetime(2022, 8, 11)
+date_range = pd.date_range(start=start_date, end=end_date, freq="D")
+raw_dir = "file_storage/raw/sales/"
 API_URL = "https://fake-api-vycpfa6oca-uc.a.run.app/sales"
 
 default_args = {
@@ -16,66 +23,82 @@ default_args = {
     "catchup": True,
 }
 
-dag = DAG(
+with DAG(
     dag_id="test_get_api_sales_result",
     description="Pipeline for processing sales data",
     default_args=default_args,
     schedule_interval="0 1 * * *",  # Every day at 1 AM UTC
     max_active_runs=1,
-)
+) as dag:
 
-start_date = datetime(2022, 8, 9)
-end_date = datetime(2022, 8, 11)
+    def get_sales(date: str, **kwargs) -> List[Dict[str, Any]]:
+        """
+        Get data from sales API for specified date.
 
-
-def get_sales(date: str) -> List[Dict[str, Any]]:
-    """
-    Get data from sales API for specified date.
-
-    :param date: Date to retrieve the data from
-    :return: List of records
-    """
-    page = 1
-    all_records = []
-
-    while True:
-        print(f"Token: {Variable.get('AUTH_TOKEN')}")
+        :param date: Date to retrieve the data from
+        :return: List of records
+        """
+        page = 1
+        all_records = []
+        task_instance = kwargs["ti"]
         auth_token = Variable.get("AUTH_TOKEN")
 
         if not auth_token:
             print("AUTH_TOKEN environment variable must be set")
 
-        response = requests.get(
-            url=API_URL,
-            headers={"Authorization": auth_token},
-            params={"date": date, "page": str(page)},
-        )
+        while True:
+            response = requests.get(
+                url=API_URL,
+                headers={"Authorization": auth_token},
+                params={"date": date, "page": str(page)},
+            )
 
-        if response.status_code == 200:
-            records = response.json()
-            all_records.extend(records)
-            page += 1
-        else:
-            print("Error API call:", response.status_code)
-            break
+            if response.status_code == 200:
+                records = response.json()
+                all_records.extend(records)
+                page += 1
+            else:
+                print("Error API call:", response.status_code)
+                break
 
-    return all_records
+        task_instance.xcom_push(key="json_data", value=all_records)
+        return all_records
 
+    def save_to_disk(date: str, **kwargs) -> None:
+        base_dir_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(base_dir_path, raw_dir, date)
+        print(path)
 
-success_task = EmptyOperator(
-    task_id="success",
-    dag=dag,
-)
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
 
-for date in [
-    start_date + timedelta(days=n) for n in range((end_date - start_date).days + 1)
-]:
-    extract_data_task = PythonOperator(
-        task_id=f"extract_data_from_api_{date.strftime('%Y-%m-%d')}",
-        python_callable=get_sales,
-        op_kwargs={"date": date.strftime("%Y-%m-%d")},
-        provide_context=True,
+        task_instance = kwargs["ti"]
+        json_content = task_instance.xcom_pull(key="json_data")
+
+        file_name = f"sales_{date}.json"
+        file_path = os.path.join(path, file_name)
+
+        with open(file_path, "w") as file:
+            json.dump(json_content, file, indent=4)
+
+    success_task = EmptyOperator(
+        task_id="success",
         dag=dag,
     )
 
-    extract_data_task >> success_task
+    for date in date_range:
+        extract_data_task = PythonOperator(
+            task_id=f"extract_data_from_api_{date.strftime('%Y-%m-%d')}",
+            python_callable=get_sales,
+            op_kwargs={"date": date.strftime("%Y-%m-%d")},
+            provide_context=True,
+        )
+
+        save_data = PythonOperator(
+            task_id=f"save_data_from_api_{date.strftime('%Y-%m-%d')}",
+            python_callable=save_to_disk,
+            op_kwargs={"date": date.strftime("%Y-%m-%d")},
+            provide_context=True,
+        )
+
+        extract_data_task >> success_task >> save_data

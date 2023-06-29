024e61c5236f96a4c0fae64a0b6c973ac95b09cb
Valentyna <ext-valentyna.lysenok@here.com>
2023-06-29 20:54:31 +0300

    HW4: add task logic to dag for job2

--- dags/demo.py
+++ dags/demo.py
@@ -3,6 +3,7 @@ import os
 from typing import List, Dict, Any
 from datetime import datetime
 import requests
+import fastavro
 import pandas as pd
 
 from airflow import DAG
@@ -24,13 +25,12 @@ default_args = {
 }
 
 with DAG(
-    dag_id="test_get_api_sales_result",
-    description="Pipeline for processing sales data",
-    default_args=default_args,
-    schedule_interval="0 1 * * *",  # Every day at 1 AM UTC
-    max_active_runs=1,
+        dag_id="test_get_api_sales_result",
+        description="Pipeline for processing sales data",
+        default_args=default_args,
+        schedule_interval="0 1 * * *",  # Every day at 1 AM UTC
+        max_active_runs=1,
 ) as dag:
-
     def get_sales(date: str, **kwargs) -> List[Dict[str, Any]]:
         """
         Get data from sales API for specified date.
@@ -64,6 +64,7 @@ with DAG(
         task_instance.xcom_push(key="json_data", value=all_records)
         return all_records
 
+
     def save_to_disk(date: str, **kwargs) -> None:
         base_dir_path = os.path.abspath(os.path.dirname(__file__))
         path = os.path.join(base_dir_path, raw_dir, date)
@@ -81,10 +82,84 @@ with DAG(
         with open(file_path, "w") as file:
             json.dump(json_content, file, indent=4)
 
+
     success_task = EmptyOperator(
         task_id="success",
         dag=dag,
     )
+
+
+    def read_json_file(json_file: str) -> Dict:
+        if not os.path.exists(json_file):
+            raise FileNotFoundError("JSON file does not exist")
+
+        with open(json_file, "r") as f:
+            json_data = json.load(f)
+
+        return json_data
+
+
+    def convert_json_data_to_avro(
+            json_data: Dict, avro_path: str, date: str
+    ) -> None:
+        os.makedirs(avro_path, exist_ok=True)
+
+        avro_file_name = f"sales_{date}.avro"
+        avro_file_path = os.path.join(avro_path, avro_file_name)
+
+        # Define the Avro schema
+        schema = {
+            "type": "record",
+            "name": "Purchase",
+            "fields": [
+                {"name": "client", "type": "string"},
+                {"name": "purchase_date", "type": "string"},
+                {"name": "product", "type": "string"},
+                {"name": "price", "type": "int"}
+            ]
+        }
+
+        # Write JSON data to Avro file
+        with open(avro_file_path, "wb") as f:
+            fastavro.writer(f, schema, json_data)
+
+        print(f"Dictionary converted to Avro: {avro_file_path}")
+
+
+    def save_sales_to_local_disk(json_file: str, avro_path: str, date: str) -> None:
+        # 1. reads and parses a JSON file
+        json_data = read_json_file(json_file=json_file)
+        # 2. save data to disk
+        convert_json_data_to_avro(
+            json_data=json_data, avro_path=avro_path, date=date
+        )
+
+
+    def main(raw_date: str):
+        base_dir_path: str = os.path.abspath(os.path.dirname(__file__))
+        raw_dir: str = "file_storage/raw/sales/2022-08-09"
+        stg_dir: str = "file_storage/stg/sales/2022-08-09"
+
+        raw_dir_path = os.path.join(
+            base_dir_path,
+            raw_dir or "file_storage/raw/sales/2022-08-09"
+        )
+        stg_dir_path = os.path.join(
+            base_dir_path,
+            stg_dir or "file_storage/stg/sales/2022-08-09"
+        )
+        json_file = os.path.join(raw_dir_path, f"sales_{raw_date}.json")
+
+        save_sales_to_local_disk(
+            json_file=json_file,
+            avro_path=stg_dir_path,
+            date=raw_date
+        )
+
+        return {
+            "message": "File transfer from json format to avro successfully.",
+        }, 201
+
 
     for date in date_range:
         extract_data_task = PythonOperator(
@@ -101,4 +176,11 @@ with DAG(
             provide_context=True,
         )
 
-        extract_data_task >> success_task >> save_data
+        convert_to_avro = PythonOperator(
+            task_id=f"convert_to_avro_{date.strftime('%Y-%m-%d')}",
+            python_callable=main,
+            op_kwargs={"raw_date": date.strftime("%Y-%m-%d")},
+            provide_context=True,
+        )
+
+        extract_data_task >> success_task >> save_data >> convert_to_avro
